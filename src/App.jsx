64612156eba676d0d959d855d0d92d3361ef5c29
Matheus Silva <matheus.vunk@gmail.com>
2024-10-04 13:45:10 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+function App() {
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<div className='container'>
+					<Routes></Routes>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;
